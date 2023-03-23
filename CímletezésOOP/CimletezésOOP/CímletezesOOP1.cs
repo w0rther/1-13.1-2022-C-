4de37dea9bd302c38fa810c294df2287ad1fc5de
pfandler.ricsi <pfandlerricsi@gmail.com>
2023-03-23 10:08:26 +0100

    kész

---
+++ CímletezésOOP/CimletezésOOP/CímletezesOOP1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CímletezésOOP
+{
+    internal class CímletezésOOP1
+    {
+        private int a;
+        private int[] b = { 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000 };
+
+        public CímletezésOOP1() { }
+
+        public CímletezésOOP1(int a)
+        {
+            this.a = a;
+        }
+        public int getA() { return this.a; }
+        public void setA(int a)
+        {
+            int mar = 0;
+            this.a = a;
+            for (int i = b.Length - 1; i >= 0; i--)
+            {
+                while (a >= b[i])
+                {
+                    a -= b[i];
+                    mar++;
+                }
+            }
+            Console.WriteLine(mar);
+        }
+
+
+
+
+    }
+
+}
---
+++ CímletezésOOP/CimletezésOOP/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CímletezésOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CímletezésOOP1 szamol = new CímletezésOOP1();
+
+            Console.WriteLine("Pénz: ");
+            int penzek = Convert.ToInt32(Console.ReadLine());
+            szamol.setA(penzek);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
