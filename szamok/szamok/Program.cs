2d9ee9fa4d438540f16c6f17b78bbe9f4b6cd7b6
pfandler.ricsi <pfandlerricsi@gmail.com>
2023-03-23 11:11:58 +0100

    szamok

--- szamok/szamok/Program.cs
+++ szamok/szamok/Program.cs
@@ -12,34 +12,14 @@ namespace szamok
         static void Main(string[] args)
         {
             Console.WriteLine("Kérek egy számot: ");
-            string n = Console.ReadLine();
+            int n = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine();
-            int hossz = n.Length;
-            int osszeg = 0;
-            for (int i = 0; i < hossz; i++) 
-            {
-                osszeg = osszeg + Convert.ToInt32(n[i]) - 48;
-            }
-            if (osszeg % 3 == 0)
-            Console.WriteLine("A szám osztható 3-mal.");
+            if (n % 3 == 0) Console.WriteLine("A szám osztható 3-mal");
             else Console.WriteLine("A szám nem osztható 3-mal");
-            if (osszeg % 9 == 0)
-            Console.WriteLine("A szám osztható 9-cel.");
+            if (n % 4 == 0) Console.WriteLine("A szám osztható 4-gyal");
+            else Console.WriteLine("A szám nem osztható 4-gyal");
+            if (n % 9 == 0) Console.WriteLine("A szám osztható 9-cel");
             else Console.WriteLine("A szám nem osztható 9-cel");
-            if (osszeg % 4 == 0)
-            Console.WriteLine("A szám osztható 4-gyel.");
-            else Console.WriteLine("A szám nem osztható 4-gyel");
-            if (hossz > 1)
-            {
-                if ((Convert.ToInt16(n[hossz-2]-48)*10+
-                    Convert.ToInt16(n[hossz-1])-48) % 4 == 0)
-                    Console.WriteLine("A szám osztható 4-gyel.");
-                    else Console.WriteLine("A szám nem osztható 4-gyel");
-
-            }
-            else if ((Convert.ToInt16(n[0]-48)% 4 ==0))
-                Console.WriteLine("A szám osztható 4-gyel.");
-                else Console.WriteLine("A szám nem osztható 4-gyel");
 
             Console.ReadLine();
         }
