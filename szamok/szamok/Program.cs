b2bfaa683fb451654380863fdeac2b266de40572
pfandler.ricsi <pfandlerricsi@gmail.com>
2023-03-23 10:47:38 +0100

    szamok

--- szamok/szamok/Program.cs
+++ szamok/szamok/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,37 @@ namespace szamok
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Kérek egy számot: ");
+            string n = Console.ReadLine();
+            Console.WriteLine();
+            int hossz = n.Length;
+            int osszeg = 0;
+            for (int i = 0; i < hossz; i++) 
+            {
+                osszeg = osszeg + Convert.ToInt32(n[i]) - 48;
+            }
+            if (osszeg % 3 == 0)
+            Console.WriteLine("A szám osztható 3-mal.");
+            else Console.WriteLine("A szám nem osztható 3-mal");
+            if (osszeg % 9 == 0)
+            Console.WriteLine("A szám osztható 9-cel.");
+            else Console.WriteLine("A szám nem osztható 9-cel");
+            if (osszeg % 4 == 0)
+            Console.WriteLine("A szám osztható 4-gyel.");
+            else Console.WriteLine("A szám nem osztható 4-gyel");
+            if (hossz > 1)
+            {
+                if ((Convert.ToInt16(n[hossz-2]-48)*10+
+                    Convert.ToInt16(n[hossz-1])-48) % 4 == 0)
+                    Console.WriteLine("A szám osztható 4-gyel.");
+                    else Console.WriteLine("A szám nem osztható 4-gyel");
+
+            }
+            else if ((Convert.ToInt16(n[0]-48)% 4 ==0))
+                Console.WriteLine("A szám osztható 4-gyel.");
+                else Console.WriteLine("A szám nem osztható 4-gyel");
+
+            Console.ReadLine();
         }
     }
 }
