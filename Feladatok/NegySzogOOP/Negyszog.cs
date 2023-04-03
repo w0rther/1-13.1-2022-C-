2a7a9867edc058adfc93ae2d726fc8c54b9a86dc
w0rther <pfandlerricsi@gmail.com>
2023-04-03 12:05:59 +0200

    saddassad

---
+++ Feladatok/NegySzogOOP/Negyszog.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NegyszogOOP
+{
+    internal class Negyszog
+    {
+        // Konstruktor
+        public int i = 0, x = 0, y = 0; //Az i, x, y tárolása
+        Random rnd = new Random(); //Random szám bekérése
+        public int ran; //A ran tárolás
+
+        public Negyszog() { }
+        public Negyszog(int i, int x, int y, int ran)
+        {
+            this.i = i;
+            this.x = x;
+            this.y = y;
+            this.ran = ran;
+        }
+        // Konzolra íratás
+        public void kiiratas()
+        {
+            while (x < 79)
+            {
+                i = 0;
+                ran = rnd.Next(2, 10);
+                while (i < ran && x < 79)
+                {
+                    i++; x++;
+                    Console.SetCursorPosition(x, y);
+                    Console.Write(" ");
+                }
+                i = 0;
+                while (i < 8 && x < 79)
+                {
+                    i++; y--;
+                    Console.SetCursorPosition(x, y);
+                    Console.Write(" ");
+                }
+                ran = rnd.Next(2, 10);
+                i = 0;
+                while (i < ran && x < 79)
+                {
+                    i++; x++;
+                    Console.SetCursorPosition(x, y);
+                    Console.Write(" ");
+                }
+                i = 0;
+                while (i < 8 && x < 79)
+                {
+                    i++; y++;
+                    Console.SetCursorPosition(x, y);
+                    Console.Write(" ");
+                }
+            }
+        }
+
+    }
+}
---
+++ Feladatok/NegySzogOOP/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NegyszogOOP
+{
+    internal class Program : Negyszog
+    {
+        static void Main(string[] args)
+        {
+            Negyszog negyszog = new Negyszog();
+            negyszog.i = 0;
+            negyszog.y = 0;
+            negyszog.x = 0;
+            negyszog.ran = 0;
+            Console.ReadKey();
+        }
+    }
+}
