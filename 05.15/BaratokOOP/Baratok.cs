d06756a41a2c92bd4189fec96082245d7b3478f9
Richárd Péter Pfandler <2022228@dszcberegszaszi.hu>
2023-05-15 14:51:01 +0200

    asdasd

---
+++ 05.15/BaratokOOP/Baratok.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BaratokOOP
+{
+    struct Szemely
+    {
+        public string nev;
+        public DateTime szido;
+        public char nem;
+        public int hajlam;
+    }
+
+
+    internal class Baratok
+    {
+        private static string path = "..\\.\\";
+        private static string inpfile = "lista.csv";
+        private static string outfile = "barat.csv";
+        private List<Szemely> list = new List<Szemely>();
+
+        #region Konstruktor
+        public Baratok() { }
+        #endregion Konstruktor
+        #region Metódusok
+        #region Beolvasás
+        public void beolvas()
+        {
+            FileStream fs = new FileStream(path + inpfile, FileMode.Create, FileAccess.Write);
+            using (StreamReader sr = new StreamReader(fs))
+            {
+                while (!sr.EndOfStream)
+                {
+                    this.list.Add(convert(sr.ReadLine().Split(';')));
+                }
+            }
+        }
+        #endregion Beolvasás
+        #region Személy
+        private Szemely convert(string[] line)
+
+        {
+            Szemely szemely = new Szemely();
+            szemely.nev = line[0];
+            szemely.szido = Convert.ToDateTime(line[1]);
+            szemely.nem = Convert.ToChar(line[2]);
+            szemely.hajlam = Convert.ToInt32(line[3]);
+
+
+            return szemely;
+        }
+        #endregion Személy
+        #region Hozzáadás
+        public void instertBarat(string[] t)
+        {
+            this.list.Add(convert(t));
+        }
+        #endregion Hozzáadás
+        #region Törlés
+
+        public void delete(string nev)
+        {
+            this.list.Remove(this.list.Find(list => list.nev == nev));
+
+        }
+        #endregion Törlés
+        #region Keresés
+
+        public bool kereses(string nev)
+        {
+            bool b = false;
+            Szemely vane = new Szemely();
+         vane = this.list.Find(list => list.nev == nev);
+            if (vane.nev==nev)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+        #endregion Keresés
+        #region Kiíratás
+
+        #endregion Kiíratás
+        #endregion Metódusok
+    }
+}
---
+++ 05.15/BaratokOOP/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaratokOOP
+{
+    internal class Program
+    {
+        private static Baratok baratok;
+        static void Main(string[] args)
+        {
+            baratok = new Baratok();
+            string[] t = { "teszt aranka", "1991.11.12", "N", "4," };
+
+            baratok.beolvas();
+            vizsga(t[0]);
+            baratok.instertBarat(t);
+            vizsga(t[0]);
+            baratok.delete(t[0]);
+            vizsga(t[0]);
+
+            Console.ReadKey();
+
+        }
+        private static void vizsga(string t)
+        {
+        if (baratok.kereses(t))
+            Console.WriteLine("Már van ilyen barátunk");
+        else 
+            Console.WriteLine("Nincs ilyen barátunk");
+            }
+
+    }
+}
