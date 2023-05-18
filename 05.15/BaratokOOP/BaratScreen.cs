77a5e9f53a51c05966ea54c008bda0245b180403
Richárd Péter Pfandler <2022228@dszcberegszaszi.hu>
2023-05-18 12:20:14 +0200

    asdas

---
+++ 05.15/BaratokOOP/BaratScreen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaratokOOP
+{
+    struct Lista
+    {
+        public string Name; // Szöveges név változó
+        public DateTime szulido; // Dátum változó DateTime-ban
+        public char nem; // Nem változó char-ban
+        public int hajlam;  // Hajlam int-be
+    }
+    internal class BaratScreen : Barat
+    {
+        #region Konstructor
+        public BaratScreen() { } // Meghívja az őskontructort
+        #endregion Konstructor
+        #region Képernyőre listázás
+        public void listazas()
+        {            
+            foreach (var item in this.list)
+            {
+                Console.WriteLine($"{item.Name, -20}; {item.szulido, 10}; {item.hajlam, 1};  {item.nem, 1}");                  
+
+            }
+        }
+        #endregion Képernyőre listázás
+    }
+}
---
+++ 05.15/BaratokOOP/BaratokPage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaratokOOP
+{
+    internal class BaratokPage
+    {
+
+
+        #region Page
+
+        public void Page()
+        {
+          
+            int lapmeret = 15;
+            int PageUp = 33;
+            int PageDown = 34;
+            int Home = 36;
+            int End = 35;
+            int Esc = 27;
+            int p;
+            int i = 0;
+            bool kilepes = false;
+            for (int i = 0; i < lapmeret; i++)
+            {
+                Console.WriteLine($"{this.list[i].Name,-20};" +
+                    $"{this.list[i].szulido.ToShortDateString(),10};" +
+                    $"{this.list[i].hajlam,1};" +
+                    $"{this.list[i].nem,-20}");
+            }
+            p = Convert.ToInt32(Console.Read);
+            switch (p)
+            {
+                case 33: // Page Up
+                    if (i < lapmeret)
+                    {
+                        i = 0;
+                    }
+                    else
+                    {
+                        i = i - lapmeret;
+                    }
+                    break;
+                default:
+                    break;
+            }
+           
+        }
+        #endregion Page
+    }
+}
