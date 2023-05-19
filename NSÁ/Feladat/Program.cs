53b734cd84db29bb44a8a7a3aeea596d263d1f15
Richárd Péter Pfandler <2022228@dszcberegszaszi.hu>
2023-05-19 11:47:27 +0200

    123123123

---
+++ NSÁ/Feladat/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maximum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adj meg egy nevet!");
+            Console.ReadLine();
+            Console.WriteLine("Adj meg egy kort!");
+
+
+            string file = Console.ReadLine();
+            using (StreamWriter fajl = new StreamWriter(file + ".txt"))
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NSÁ/Gomb/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gomb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nyomj egy gombot");
+            ConsoleKeyInfo=Console.ReadKey();
+            Console.WriteLine("\n");
+            if (k.Key == ConsoleKey.Home) 
+            {
+                Console.WriteLine("Megnyomtad a homeot");
+               
+            }
+        }
+    }
+}
