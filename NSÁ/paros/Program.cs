34a4dfcce349424b995691ae8ccda297da38ca23
Richárd Péter Pfandler <2022228@dszcberegszaszi.hu>
2023-05-23 10:52:47 +0200

    asdas

---
+++ NSÁ/paros/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paros
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
